154e63d25faa7be8c4292c5ee4aaec37d1462d65
Saja Alhimiary <sajaalhimiary@yahoo.com>
2023-03-27 21:28:37 +0300

    adding heroku build files

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,16 +8,33 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
-    try routes(app)
-    app.databases.use(
-        .postgres(hostname: "localhost",
-                  username: "shahadmohammed",
-                  password: "vapor",database: "appstoreratings"),
-               as: .psql)
+    if let urlString = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: urlString){
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgresConfig.tlsConfiguration = tlsConfig
+        
+        app.databases.use(.postgres(configuration: postgresConfig), as:
+                .psql)
+    } else {
+        app.databases.use (.postgres (
+            hostname: Environment.get ("DATABASE_HOST") ?? "localhost",
+            port:
+                Environment.get("DATABASE_PORT").flatMap (Int.init(_:))
+            ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ??
+            "shahadmohammed",
+            password: Environment.get("DATABASE_PASSWORD") ??
+            "vapor",
+            database: Environment.get("DATABASE_NAME") ??
+            "appstoreratings" ) , as: .psql)
+    }
     
     //Migration Config
     app.migrations.add(Createplatform())
     app.migrations.add(Creategames())
+    
+    try routes(app)
 
 }
 
